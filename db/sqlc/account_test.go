9ac313dc12b8a284275675b776cd2052fd071c65
Thanathip <dannajaeiei@hotmail.com>
2022-07-02 20:56:08 +0700

    Fixed TestCreateAccount to using random value

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"testing"
 
+	"github.com/danyouknowme/simplebank/util"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "Danny",
-		Balance:  1500,
-		Currency: "BAHT",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)

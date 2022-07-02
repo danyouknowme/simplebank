472fa258a919833386e0ff8567a14bb2b41fbea9
Thanathip <dannajaeiei@hotmail.com>
2022-07-02 21:07:48 +0700

    Crete test function DeleteAccount

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 	"time"
 
@@ -64,4 +65,16 @@ func TestUpdateAccount(t *testing.T) {
 	require.Equal(t, randomAccount.ID, account.ID)
 	require.Equal(t, randomAccount.ID, account.ID)
 	require.WithinDuration(t, randomAccount.CreatedAt, account.CreatedAt, time.Second)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	randomAccount := createRandomAccount(t)
+
+	err := testQueries.DeleteAccount(context.Background(), randomAccount.ID)
+	require.NoError(t, err)
+
+	account, err := testQueries.GetAccount(context.Background(), randomAccount.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, account)
 }

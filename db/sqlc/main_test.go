694be9d282d79de34ae2bc82a1c37f646a0e1037
Thanathip <dannajaeiei@hotmail.com>
2022-07-03 12:34:35 +0700

    Added variable testDB to get databse driver

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,18 +11,21 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "user=root password=secret dbname=simple_bank sslmode=disable"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("Cannot connect to database", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }

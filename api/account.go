a899bef8070b55b12f81711fd6028f94c2c82315
Thanathip <dannajaeiei@hotmail.com>
2022-07-10 17:26:13 +0700

    Refactoring spacing of struct

--- api/account.go
+++ api/account.go
@@ -61,7 +61,7 @@ func (server *Server) getAccount(ctx *gin.Context) {
 }
 
 type GetListAccountsRequest struct {
-	PageID int32 `form:"page_id" binding:"required,min=1"`
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
 	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
@@ -73,7 +73,7 @@ func (server *Server) getListAccounts(ctx *gin.Context) {
 	}
 
 	arg := db.GetListAccountsParams{
-		Limit: req.PageSize,
+		Limit:  req.PageSize,
 		Offset: (req.PageID - 1) * req.PageSize,
 	}
 

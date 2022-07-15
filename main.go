d77a05eb8ad64c3e371b89275e46be646a277cd7
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-07-15 23:09:28 +0700

    Fixed argrument new server api

--- main.go
+++ main.go
@@ -13,18 +13,21 @@ import (
 func main() {
 	config, err := util.LoadConfig(".")
 	if err != nil {
-		log.Fatal("Connot load config", err)
+		log.Fatal("Connot load config:", err)
 	}
 	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
-		log.Fatal("Cannot connect to database", err)
+		log.Fatal("Cannot connect to database:", err)
 	}
 
 	store := db.NewStore(conn)
-	server := api.NewServer(store)
+	server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal("Cannot create server:", err)
+	}
 
 	err = server.Start(config.ServerAddress)
 	if err != nil {
-		log.Fatal("Cannot start server", err)
+		log.Fatal("Cannot start server:", err)
 	}
 }

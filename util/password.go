e65733f8310905ca27c3d2948f58e7140d3980a9
Thanathip <dannajaeiei@hotmail.com>
2022-07-17 19:38:02 +0700

    Added verify password condition before hash

--- util/password.go
+++ util/password.go
@@ -2,13 +2,14 @@ package util
 
 import (
 	"fmt"
+	"unicode"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 func HashPassword(password string) (string, error) {
-	if len(password) < 6 {
-		return "", fmt.Errorf("password should be at least 6 characters")
+	if !PasswordCondition(password) {
+		return "", fmt.Errorf("password not match with the condition")
 	}
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
@@ -20,4 +21,40 @@ func HashPassword(password string) (string, error) {
 
 func CheckPassword(password string, hashedPassword string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func PasswordCondition(password string) bool {
+	var (
+		upp, low, num, sym bool
+		length             uint8
+	)
+
+	for _, char := range password {
+		switch {
+		case unicode.IsUpper(char):
+			// uppercase character
+			upp = true
+			length++
+		case unicode.IsLower(char):
+			// lowercase character
+			low = true
+			length++
+		case unicode.IsNumber(char):
+			// numeric character
+			num = true
+			length++
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			// special character
+			sym = true
+			length++
+		default:
+			return false
+		}
+	}
+
+	if !upp || !low || !num || !sym || length < 6 {
+		return false
+	}
+
+	return true
 }

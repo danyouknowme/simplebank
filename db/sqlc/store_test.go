bf98b277b1288cfe01675517cc87350fdf088bf9
Thanathip <dannajaeiei@hotmail.com>
2022-07-17 20:17:50 +0700

    Use TransferTxParams instead CreateTransferParams in mock store

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -24,7 +24,7 @@ func TestTransferTx(t *testing.T) {
 
 	for i := 0; i < n; i++ {
 		go func() {
-			result, err := store.TransferTx(context.Background(), CreateTransferParams{
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
 				FromAccountID: account1.ID,
 				ToAccountID:   account2.ID,
 				Amount:        amount,
@@ -137,7 +137,7 @@ func TestTransferTxDeadLock(t *testing.T) {
 			toAccountID = account1.ID
 		}
 		go func() {
-			_, err := store.TransferTx(context.Background(), CreateTransferParams{
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
 				FromAccountID: fromAccountID,
 				ToAccountID:   toAccountID,
 				Amount:        amount,

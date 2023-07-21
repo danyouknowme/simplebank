030946c12dd136f18bf389b626f31877ed1366e3
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2023-07-21 16:33:50 +0700

    Added user_agent and client_ip in create session

--- api/user.go
+++ api/user.go
@@ -133,8 +133,8 @@ func (server *Server) loginUser(ctx *gin.Context) {
 		ID:           refreshTokenPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    ctx.Request.UserAgent(),
+		ClientIp:     ctx.ClientIP(),
 		IsBlocked:    false,
 		ExpiresAt:    refreshTokenPayload.ExpiredAt,
 	})
